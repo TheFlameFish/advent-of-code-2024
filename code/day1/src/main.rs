5bdd5d24ea4caee89d8f63c93cbf040a3b4a2c67
FlameFish <mccoa499@gmail.com>
2024-12-05 12:01:52 -0800

    Init and also started day 1

---
+++ code/day1/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    // Create a path to the desired file
+    let path = Path::new("../../inputs/day1.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} has been read succesfully.", display),
+    }
+}
+
+fn seperate(s: String) -> (Vec<i32>, Vec<i32>) {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+}
